a86e363e7209bfab024855a5bb34cdc03977a03d
ramanand-n <ramanand.nandakumar@trenser.com>
2023-09-20 12:11:57 +0530

    first commit

---
+++ FileManager/FileManager.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <string_view>
+#include <cstddef>
+#include <iostream>
+#include <filesystem>
+using  namespace std;
+namespace fs = filesystem;
+
+int main()
+{
+    int choice;
+    string Currentpath = "D:\\";
+
+    cout << "1. List Files\n2. Change Directory\n3. Copy Files\n4. Move Files\n5. Delete Files";
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        for (const auto& entry : fs::directory_iterator(Currentpath))
+        {
+           string StringPath = entry.path().generic_string();
+            //std::string stringpath = entry.path();
+           // stringpath.substr(stringpath.find("/") + 1);
+            cout << StringPath << endl;
+           
+        }
+        break;
+    }
+}
